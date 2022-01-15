7b583229c68e256f8364eb25073b27bb9c7148ef
apapacharal <97394826+apapacharal@users.noreply.github.com>
2022-01-15 19:42:45 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index2 = 0; index2 <= 4; index2++) {
+        basic.pause(1000)
+        led.toggle(index2, 1)
+    }
+    for (let index2 = 0; index2 <= 4; index2++) {
+        basic.pause(1000)
+        led.toggle(index2, 1)
+    }
+})
 led.plot(0, 0)
 basic.forever(function () {
-    for (let n = 0; n <= 4; n++) {
-        for (let index2 = 0; index2 <= 4; index2++) {
-            basic.pause(1000)
-            led.toggle(index2, 1)
-        }
-        for (let index2 = 0; index2 <= 4; index2++) {
-            basic.pause(1000)
-            led.toggle(index2, 1)
-        }
-        led.toggle(n, 2)
-    }
+	
 })
